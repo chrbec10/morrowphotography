541aea535e8ee476c8578589f40d243b1458af5a
keybecker <82342623+keybecker@users.noreply.github.com>
2021-06-17 12:36:31 +1200

    includes setup, initial homepage css

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+$title = "Home";
+include_once("includes/header.php");
+include_once("includes/navigation.php");
+?>
+
+<div class="parallax hero">
+    <div class="title">
+        <span class="border">THIS IS A TEST</span>
+    </div>
+</div>
+
+<?php
+include_once("includes/footer.php");
