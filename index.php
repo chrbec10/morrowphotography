4f65a3015563ab6a65f3033352ac51942cc18802
keybecker <82342623+keybecker@users.noreply.github.com>
2021-06-17 14:06:07 +1200

    Signature update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once("includes/navigation.php");
 
 <div class="parallax" id="hero" style="background-image: url('S_7100_-0605.jpg')">
     <div class="title">
-        <span class="border">THIS IS A TEST</span>
+        <img src="CM Signature.png">
     </div>
 </div>
 
