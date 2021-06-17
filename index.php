d2a600e208516bda441b815fb23b134c07bb6517
keybecker <82342623+keybecker@users.noreply.github.com>
2021-06-17 12:45:41 +1200

    homepage hero

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once("includes/header.php");
 include_once("includes/navigation.php");
 ?>
 
-<div class="parallax hero">
+<div class="parallax" id="hero">
     <div class="title">
         <span class="border">THIS IS A TEST</span>
     </div>
