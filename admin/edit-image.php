8a388e5a6acb38e4a7d162a8edf1a7964951618e
keybecker <82342623+keybecker@users.noreply.github.com>
2021-06-20 15:27:22 +1200

    upload-image complete

--- admin/edit-image.php
+++ admin/edit-image.php
@@ -35,14 +35,14 @@ if (isset($_GET['id']) && !empty(trim($_GET['id']))){
                 $image = $row['image'];
                 $description = $row['description'];
 
-                $sql = "SELECT tags_rel.tag_ID, tags_list.tag FROM tags_rel INNER JOIN tags_list ON tags_list.ID = tags_rel.tag_ID WHERE tags_rel.image_ID = :image_ID";
+                $sql = "SELECT tag_ID FROM tags_rel WHERE image_ID = :image_ID";
                 if ($stmt = $pdo->prepare($sql)){
                     $stmt->bindParam(":image_ID", $param_image_ID);
 
                     $param_image_ID = $image_ID;
 
-                    if ($stmt->execure()){
-                        $tags = json_encode( $stmt->fetchAll(PDO::FETCH_NUM) );
+                    if ($stmt->execute()){
+                        $tags_ID = $stmt->fetchAll();
                     }
                 }
 
@@ -70,5 +70,78 @@ if (isset($_POST['id']) && !empty(trim($_POST['id']))){
     
 }
 
-
+//Retrieve tags list to populate Select dropdown
+$sql = "SELECT * FROM tags_list";
+if($stmt = $pdo->query($sql)) {
+    $tags_array[] = $stmt->fetchAll();
+}
 ?>
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<?php
+include_once('../includes/navbar.php');
+?>
+<div class="page-container">
+    <main class="main">
+        <div class="container">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <input type="file" name="image" id="image" class="form-control <?php echo (!empty($image_err)) ? 'is-invalid' : ''; ?>" aria-describedby="fileHelp">
+                    <div id="fileHelp" class="form-text">File must be a .jpg, .jpeg, .gif, or .png file, and less than 2MB in size.</div> 
+                    <span class="invalid-feedback"><?php echo $image_err;?></span>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="title">Image Title</label>
+                    <input type="text" class="form-control <?php echo (!empty($title_err)) ? 'is-invalid' : ''; ?>" name="title" id="title" value="<?php echo $title; ?>">
+                    <span class="invalid-feedback"><?php echo $title_err;?></span>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="description">Image Description</label>
+                    <textarea class="form-control <?php echo (!empty($description_err)) ? 'is-invalid' : ''; ?>" name="description" id="description"><?php echo $description; ?></textarea>
+                    <span class="invalid-feedback"><?php echo $description_err;?></span>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="twitter">Twitter Link</label>
+                    <input type="text" class="form-control <?php echo (!empty($twitter_err)) ? 'is-invalid' : ''; ?>" name="twitter" id="twitter" value="<?php echo $twitter; ?>">
+                    <span class="invalid-feedback"><?php echo $twitter_err;?></span>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="facebook">Facebook Link</label>
+                    <input type="text" class="form-control <?php echo (!empty($facebook_err)) ? 'is-invalid' : ''; ?>" name="facebook" id="facebook" value="<?php echo $facebook; ?>">
+                    <span class="invalid-feedback"><?php echo $facebook_err;?></span>
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="tags">Search Tags</label>
+                    <select class="tags-select form-control <?php echo (!empty($tags_err)) ? 'is-invalid' : ''; ?>" name="tags[]" id="tags" multiple="multiple">
+                    <?php
+                    foreach($tags_array as $tag_sub){
+                        foreach($tag_sub as $tag){
+                            echo"<option value='" . $tag['ID'] . "'>" . $tag['tag'] . "</option>";
+                        }
+                    }
+                    ?>
+                    </select>
+                    <span class="invalid-feedback"><?php echo $tags_err;?></span>
+                </div>
+                <br>
+                <button type="submit" class="btn btn-dark btn-submit">Submit</button>
+            </form>
+        </div>
+    <?php
+    include_once('../includes/footer.php');
+    ?>
+    </main>
+</div>
+<script>
+$(document).ready(function() {
+    $('.tags-select').select2();
+});
+</script>
+</body>
--- admin/upload-image.php
+++ admin/upload-image.php
@@ -44,8 +44,9 @@ function compressImage($source, $name, $quality, $filepath) {
 
 
 //Define variables
-$title = $description = $twitter = $facebook = $tags = '';
-$title_err = $description_err = $twitter_err = $facebook_err = $tags_err = '';
+$title = $description = $twitter = $facebook = '';
+$tags = [];
+$title_err = $description_err = $twitter_err = $facebook_err = $tags_err = $image_err = '';
 
 //Process form data on submit
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -87,9 +88,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $facebook_err = "Please enter a facebook link";
     }
 
-    $input_tags = trim($_POST['tags']);
-    if (!empty($input_tags)) {
-        $tags = $input_tags;
+    if (!empty($_POST['tags'])) {
+        $tags = $_POST['tags'];
     } else {
         $tags_err = "Please select at least one tag for the image";
     }
@@ -107,36 +107,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $quality = 90;
 
         //Get information about file
-        $filesize = filesize($_FILES["image"]["name"]);
+        $filesize = filesize($_FILES["image"]["tmp_name"]);
         $filetype = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
         $filename = $_FILES["image"]["name"];
         $error = $_FILES["image"]["error"];
 
         //Check filesize
         if ($filesize > $maxsize) {
-            die(echo "file too large");
+            $image_err = 'File too large. Please upload a file that is less than 2MB in size.';
         
         //Check errors
         } else if ($error != 0){
-            die(echo "erorr code: " . $error);
+            $image_err = 'Error uploading file. Error code: ' . $error;
         
         //Check filetype
         } else if(!in_array(strtolower($filetype), $allowed)) {
-            die(echo "wrong filetype");
+            $image_err = 'File is wrong filetype. Please upload a .jpg, .jpeg, .gif, or .png file.';
         
         //If no problems
         } else {
             //Select image, create image name
             $image = $_FILES["image"]["tmp_name"];
             $image_name = hash_file('sha1', $image) . '.jpg';
-            compressImage($image, $image_name, $quality, $destination);
+            if (!file_exists($destination . '/' . $image_name)){
+                compressImage($image, $image_name, $quality, $destination);
+            }
         }
+    } else if(!isset($_FILES["image"])) {
+        $image_err = 'Please choose an image to upload.';
 
-
+    } else {
+        $image_err = 'Error uploading file. Error code: ' . $_FILES["image"]["error"];
     }
 
     //If there aren't any errors
-    if (empty($title_err) && empty($description_err) && empty($twitter_err) && empty($facebook_err)) {
+    if (empty($title_err) && empty($description_err) && empty($twitter_err) && empty($facebook_err) && empty($image_err) && empty($tags_err)) {
 
         //Create SQL query
         $sql = "INSERT INTO images(title, twitter, facebook, image, description) VALUES (:title, :twitter, :facebook, :image, :description)";
@@ -166,23 +171,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if ($stmt = $pdo->query($sql)){
 
                     $created = ($stmt->fetchColumn());
-
                     //Ready a statement to insert tags
                     $sql = "INSERT INTO tags_rel(image_ID, tag_ID) VALUES";
                     
                     //Build an insert row for each tag
                     //TODO: Setup tag array
                     foreach($tags as $tag) {
+                        
                         $sql .= "(" . $created . "," . $tag . "),";
                     };
 
                     //Trim last comma from SQL query
-                    rtrim($sql, ",");
+                    $sql = substr($sql, 0, -1);
                     $sql .= ";";
-                    
                     //If our tags are inserted successfully
                     if($stmt=$pdo->query($sql)) {
-                        //TODO: Success message
+                        header("location: edit-image.php?id=" . $created);
                     }
                 }
             }
@@ -190,21 +194,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+//Retrieve tags list to populate Select dropdown
 $sql = "SELECT * FROM tags_list";
 if($stmt = $pdo->query($sql)) {
-    $tags_array = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+    $tags_array[] = $stmt->fetchAll();
 }
 
+unset($stmt);
+unset($pdo);
+?>
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+
+<?php
 include_once('../includes/navbar.php');
 ?>
 <div class="page-container">
     <main class="main">
         <div class="container">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form data">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
                 <div class="form-group">
-                    <input type="hidden" name="MAX_FILE_SIZE" value="2097152">
-                    <input type="file" name="image" id="image" class="form-control" aria-describedby="fileHelp">
+                    <input type="file" name="image" id="image" class="form-control <?php echo (!empty($image_err)) ? 'is-invalid' : ''; ?>" aria-describedby="fileHelp">
                     <div id="fileHelp" class="form-text">File must be a .jpg, .jpeg, .gif, or .png file, and less than 2MB in size.</div> 
+                    <span class="invalid-feedback"><?php echo $image_err;?></span>
                 </div>
                 <br>
                 <div class="form-group">
@@ -215,7 +229,7 @@ include_once('../includes/navbar.php');
                 <br>
                 <div class="form-group">
                     <label for="description">Image Description</label>
-                    <input type="text" class="form-control <?php echo (!empty($description_err)) ? 'is-invalid' : ''; ?>" name="description" id="description" value="<?php echo $description; ?>">
+                    <textarea class="form-control <?php echo (!empty($description_err)) ? 'is-invalid' : ''; ?>" name="description" id="description"><?php echo $description; ?></textarea>
                     <span class="invalid-feedback"><?php echo $description_err;?></span>
                 </div>
                 <br>
@@ -231,6 +245,20 @@ include_once('../includes/navbar.php');
                     <span class="invalid-feedback"><?php echo $facebook_err;?></span>
                 </div>
                 <br>
+                <div class="form-group">
+                    <label for="tags">Search Tags</label>
+                    <select class="tags-select form-control <?php echo (!empty($tags_err)) ? 'is-invalid' : ''; ?>" name="tags[]" id="tags" multiple="multiple">
+                    <?php
+                    foreach($tags_array as $tag_sub){
+                        foreach($tag_sub as $tag){
+                            echo"<option value='" . $tag['ID'] . "'>" . $tag['tag'] . "</option>";
+                        }
+                    }
+                    ?>
+                    </select>
+                    <span class="invalid-feedback"><?php echo $tags_err;?></span>
+                </div>
+                <br>
                 <button type="submit" class="btn btn-dark btn-submit">Submit</button>
             </form>
         </div>
@@ -239,5 +267,10 @@ include_once('../includes/navbar.php');
     ?>
     </main>
 </div>
+<script>
+$(document).ready(function() {
+    $('.tags-select').select2();
+});
+</script>
 </body>
 
